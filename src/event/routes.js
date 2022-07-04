a3dfcb16dc0f0f0e380f2aedd6652744e27e734b
Richard <isit_ric@hotmail.com>
2022-07-04 17:26:11 +0100

    ric-event attendees

--- src/event/routes.js
+++ src/event/routes.js
@@ -8,7 +8,7 @@ eventRouter.post("/event/create", createEvent);
 eventRouter.post("/attendee/remove", removeAttendee);
 
 eventRouter.get("/event/:id", async (req, res) => {
-    const event = await Event.findOne({where: { event_id: req.params.id.substring(1) }, attributes: ["event_id", "attendees"]});
+    const event = await Event.findOne({where: { event_id: req.params.id }, attributes: ["event_id", "attendees"]});
     res.status(200).json(event);
 });
 
