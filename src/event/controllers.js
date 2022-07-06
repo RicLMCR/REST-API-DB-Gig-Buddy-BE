799715036feb95f3a4dc45834c23c74e6470f592
Ibrahim <ibrahim.345xa@slmail.me>
2022-07-06 11:02:28 +0100

    rename events_attending to eventsAttending

--- src/event/controllers.js
+++ src/event/controllers.js
@@ -9,15 +9,15 @@ exports.createEvent = async (req, res) => {
     try {
         const existingEvent = await Event.findOne({where: { event_id: req.body.eventId }});
         const existingUser = await User.findOne({where: { username: req.body.username }});
-        let events_attending = existingUser.events_attending;
+        let eventsAttending = existingUser.eventsAttending;
         if (existingUser === null) {
             res.status(400).json({error: "user not found in database"});
         }else if (existingEvent === null) {
             // create event if it doesn't exist in database and add user to attendees list
             const userEvent = await Event.create({event_id: req.body.eventId, attendees: [req.body.username]});
-            // update user events_attending list
-            events_attending.push(req.body.eventId);
-            await User.update({events_attending: events_attending}, { where: {username: req.body.username}});
+            // update user eventsAttending list
+            eventsAttending.push(req.body.eventId);
+            await User.update({eventsAttending: eventsAttending}, { where: {username: req.body.username}});
             res.send( {userEvent});
         } else {
             // update existing event of attendees
@@ -27,9 +27,9 @@ exports.createEvent = async (req, res) => {
                 res.status(400).json({error: "user is already attending event"});
             } else {
                 attendees.push(req.body.username);
-                events_attending.push(req.body.eventId);
+                eventsAttending.push(req.body.eventId);
                 await Event.update({attendees: attendees }, { where: {event_id: req.body.eventId}});
-                await User.update({events_attending: events_attending}, { where: {username: req.body.username}});
+                await User.update({eventsAttending: eventsAttending}, { where: {username: req.body.username}});
                 res.status(200).json({userEvent: existingEvent});
             }
         }
@@ -52,7 +52,7 @@ exports.removeAttendee = async (req, res) => {
         const existingEvent = await Event.findOne({where: { event_id: req.body.eventId }});
         let attendees = existingEvent.attendees;
         const existingUser = await User.findOne({where: { username: req.body.username }});
-        let events_attending = existingUser.events_attending;
+        let eventsAttending = existingUser.eventsAttending;
         if (!attendees.includes(req.body.username)){
             // returns error if user is not in attendees list
             res.status(400).json({error: "user is not attending event"});
@@ -60,9 +60,9 @@ exports.removeAttendee = async (req, res) => {
             // find position of attendee and remove them
             attendees.splice(attendees.indexOf(req.body.username), 1);
             await Event.update({attendees: attendees }, { where: {event_id: req.body.eventId}});
-            // same for their own events_attending list
-            events_attending.splice(events_attending.indexOf(req.body.eventId), 1);
-            await User.update({events_attending: events_attending }, {where: {username: req.body.username}});
+            // same for their own eventsAttending list
+            eventsAttending.splice(eventsAttending.indexOf(req.body.eventId), 1);
+            await User.update({eventsAttending: eventsAttending }, {where: {username: req.body.username}});
             res.status(200).json({userEvent: existingEvent });
         }
     } catch (error) {
