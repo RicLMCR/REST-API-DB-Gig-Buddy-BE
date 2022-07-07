cdbc635608e8125b727eb7f2c64fdd13071e0dba
MdotIbrahim <94474548+MdotIbrahim@users.noreply.github.com>
2022-07-07 17:34:53 +0100

    Revert "connection test"

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,11 +2,4 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 console.log("Succesfully connected to db");
-exports.sequelize = new Sequelize(`${process.env.DATABASE_URL}?sslmode=require`, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            rejectUnauthorized: false,
-        }
-    }
-});
+exports.sequelize = new Sequelize(process.env.DATABASE_URL);
