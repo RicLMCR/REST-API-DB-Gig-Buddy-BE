ee0ab1b8cbf64373842fcfccc97c49123589d166
Ibrahim <ibrahim.345xa@slmail.me>
2022-07-07 16:03:24 +0100

    serverjs test

--- src/server.js
+++ src/server.js
@@ -5,25 +5,25 @@ const express = require ("express");
 const cors = require ("cors");
 const app = express();
 const port = process.env.PORT || 5001;
-const userRouter = require("./user/routes");
-const eventRouter = require("./event/routes");//
+// const userRouter = require("./user/routes");
+// const eventRouter = require("./event/routes");
 const { sequelize } = require("./db/connection");
-const User = require("./user/model");
-const Event = require("./event/model")
+// const User = require("./user/model");
+// const Event = require("./event/model")
 
 
 app.use(express.json());
 app.use(cors());
-app.use(userRouter);
-app.use(eventRouter);
-console.log("server working");
+// app.use(userRouter);
+// app.use(eventRouter);
 
-// app.get("/",(req,res)=>{
-//     res.send("Hello world");
-// })
+
+app.get("/",(req,res)=>{
+    res.send("Hello world");
+})
 
 app.listen(port, async ()=>{
-    await User.sync({alter:true})
-    await Event.sync({alter:true}) // All new tables need to be added here
+    // await User.sync({alter:true})
+    // await Event.sync({alter:true}) // All new tables need to be added here
     console.log(`App is listening on ${port}`);
 })
